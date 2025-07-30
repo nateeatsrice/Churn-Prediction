5fcc5a285a90731a38bc4935ab24616e2335397e
nateeatsrice <nathaniel.c.rice@gmail.com>
2025-07-30 17:10:59 -0500

    Added train.py

---
+++ train.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+
+from sklearn.feature_extraction import DictVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import confusion_matrix
+
+import pickle
+
+#data wrangling and format standardization
+df = pd.read_csv('Telco-Customer-Churn.csv')
+
+df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+df['TotalCharges'] = df['TotalCharges'].fillna(0)
+
+df.columns = df.columns.str.lower().str.replace(' ', '_')
+
+string_columns = list(df.dtypes[df.dtypes == 'object'].index)
+
+for col in string_columns:
+    df[col] = df[col].str.lower().str.replace(' ', '_')
+
+df.churn = (df.churn == 'yes').astype(int)
+
+#split full training and test set
+df_train_full, df_test = train_test_split(df, test_size=0.2, random_state=1)
+
+df_train_full = df_train_full.reset_index(drop=True)
+df_test = df_test.reset_index(drop=True)
+
+categorical = ['gender', 'seniorcitizen', 'partner', 'dependents',
+               'phoneservice', 'multiplelines', 'internetservice',
+               'onlinesecurity', 'onlinebackup', 'deviceprotection',
+               'techsupport', 'streamingtv', 'streamingmovies',
+               'contract', 'paperlessbilling', 'paymentmethod']
+numerical = ['tenure', 'monthlycharges', 'totalcharges']
+
+def train(df, y, C):
+    # Dict vectorizer inputs a dictionary
+    cat = df[categorical + numerical].to_dict(orient='records')
+
+    dv = DictVectorizer(sparse=False)
+    dv.fit(cat)
+
+    X = dv.transform(cat)
+
+    model = LogisticRegression(solver='liblinear', C=C)
+    model.fit(X, y)
+
+    return dv, model
+
+def predict(df, dv, model):
+    cat = df[categorical + numerical].to_dict(orient='records')
+
+    X = dv.transform(cat)
+
+    y_pred = model.predict_proba(X)[:, 1]
+
+    return y_pred
+
+# seeing that the best model was with C= 0.1 we now train on full training set (training+val) and compare with test
+y_train = df_train_full.churn.values
+y_test = df_test.churn.values
+C = 0.1
+# the model and dv below are ultimitly the file we want to export
+dv, model = train(df_train_full, y_train, C=C)
+
+# same as code above, './churn-model.bin' names the file and directory
+with open('./churn-model.bin', 'wb') as f_out:
+    pickle.dump((dv, model), f_out)
+
+import requests
+url = 'http://localhost:9696/predict'
+response = requests.post(url, json=customer)
+result = response.json()
+result
